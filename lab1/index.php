2e827a2f9b188ebc9ea1eb54512d8d7a4b811f2c
makarovilia <rety2683@gmail.com>
2026-05-08 14:02:57 +0300

    Lab 1

---
+++ lab1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="main/main.css">
+</head>
+<body>
+    <header class="bg-dark text-white p-3">
+        <img src="main/logo.png" alt="">
+    </header>
+    <main>
+        <p>
+            <?="Hello world"?>
+        </p>
+    </main>
+    <footer class="bg-dark text-white text-center p-3 ft">
+        <p>Задание для самостоятельной работы</p>
+    </footer>
+</body>
+</html>

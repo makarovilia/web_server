65bc23fe58ee8c9475bde9de93aa8185dd8510d7
makarovilia <rety2683@gmail.com>
2026-06-06 22:10:29 +0300

    Task 3.2

---
+++ Task/templates/footer.php
@@ -0,0 +1,5 @@
+    <footer class="bg-dark text-white text-center p-3 ft">
+        <p>Задание для самостоятельной работы</p>
+    </footer>
+</body>
+</html>
---
+++ Task/trig.php
@@ -0,0 +1,7 @@
+<?php
+
+function trig($func, $value)
+{
+    return $func(deg2rad($value));
+}
+?>
